3175d1fcf00cfdcf7cf8c18b7c129c286dad672a
Kevinectv <34521425+kevinectv@users.noreply.github.com>
2023-07-05 23:41:25 -0400

    Avatar

--- avatar.js
+++ avatar.js
@@ -1,8 +1,10 @@
 const { EmbedBuilder, ApplicationCommandType } = require("discord.js");
 
+
 /**
  * @type {import('@structures/BaseContext')}
  */
+
 module.exports = {
   name: "avatar",
   description: "displays avatar information about the user",
