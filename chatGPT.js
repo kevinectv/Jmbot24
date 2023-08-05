b3b06826dd642c0d2bcfac37a74533b995c9fe06
Kevinectv <34521425+kevinectv@users.noreply.github.com>
2023-08-05 14:17:21 -0500

    Update chatGPT.js

--- chatGPT.js
+++ chatGPT.js
@@ -17,7 +17,44 @@ module.exports = {
     
  
 }
+module.exports = {
+    name: "chat-gpt",
+    description: "Puedes preguntar algo a chat GPT",
+    StringOption: option 
+        name: "pregunta",
+        Description: "Escribe la pregunta que deses que te responda la IA",
+        .setMaxLength(300)
+        .setRequired(true)
+    ),
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async execute(interaction){
+        const {options} = interaction
+        const pregunta = options.getString('pregunta')
+        try {
+            const res = await openai.createCompletion({
+                model:'text-davinci-003',
+                prompt: pregunta,
+                max_tokens: 2048,
+                temperature:0.5,
+            })
 
+            const embed = new EmbedBuilder()
+            .setTitle('PREGUNTA A CHAT GPT')
+            .setAuthor({name:`${interaction.user.tag} Acaba de hacer una pregunta a CHAT-GPT`, iconURL: interaction.user.avatarURL({dynamic:true})})
+            .setColor('Random')
+            .setDescription(`Pregunta: \`\`\`${pregunta}\`\`\`\n\n Respuesta: \`\`\`${res.data.choices[0].text}\`\`\` `)
+
+            return await interaction.reply({embeds:[embed]})
+        } catch (error) {
+            console.log(error);
+            return errReply(interaction,"Se produjo un error al tratar de realizar este comando",true)
+        }
+    }
+    
+};
 
 
 
