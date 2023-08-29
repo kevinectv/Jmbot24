c11b22493d266b9cdfa92353e93c6e4d9f719770
Kevinectv <34521425+kevinectv@users.noreply.github.com>
2023-08-29 13:36:39 -0400

    index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ require('dotenv/config');
 const { Client } = require('discord.js');
 const { OpenAI } = require('openai');
 
+const Client = new Client({
+    intents: ['Guilds', 'GuildMembers', 'GuildMessages']
+
+});
+
